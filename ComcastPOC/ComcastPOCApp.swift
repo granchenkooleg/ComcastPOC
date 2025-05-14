7434447e673f03a66f17cb0a101cf991081a109a
Oleg <granchenkooleg@gmail.com>
2025-05-14 16:00:44 +0300

    Initial Commit

---
+++ ComcastPOC/ComcastPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComcastPOCApp.swift
+//  ComcastPOC
+//
+//  Created by Oleg Granchenko on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ComcastPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
